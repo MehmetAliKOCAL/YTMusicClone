e05574ed9fe966202a6ca1aa5a92568d9975a944
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-04-27 23:16:06 +0300

    initial commit

---
+++ components/categories.vue
@@ -0,0 +1,37 @@
+<script setup>
+const categories = [
+  {
+    name: "Energize",
+    link: "/",
+  },
+  {
+    name: "Relax",
+    link: "/",
+  },
+  {
+    name: "Workout",
+    link: "/",
+  },
+  {
+    name: "Focus",
+    link: "/",
+  },
+  {
+    name: "Commute",
+    link: "/",
+  },
+];
+</script>
+
+<template>
+  <div class="flex gap-3 py-4 overflow-x-auto categoriesScrollbar">
+    <NuxtLink
+      v-for="category in categories"
+      :key="category.name"
+      :to="category.link"
+      class="rounded-lg bg-white/10 hover:bg-white/20 focus:bg-white/30 transition-colors duration-200 text-white text-sm px-3 py-2"
+    >
+      {{ category.name }}
+    </NuxtLink>
+  </div>
+</template>
---
+++ components/icons/delete.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg viewBox="0 0 24 24" class="invert-[50%]">
+    <g>
+      <path
+        d="M11,17H9V8h2V17z M15,8h-2v9h2V8z M19,4v1h-1v16H6V5H5V4h4V3h6v1H19z M17,5H7v15h10V5z"
+      ></path>
+    </g>
+  </svg>
+</template>
