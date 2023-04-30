e57bcc816f86b7f1d0f4bafe00d38603e0130d23
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-04-30 20:07:40 +0300

    more width options added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 export default <Partial<Config>>{
-    theme: {
+  theme: {
     extend: {
       colors: {
-        ytblack: 'rgb(3,3,3)',
-        ytgray: 'rgb(129,129,129)'
+        ytblack: "rgb(3,3,3)",
+        ytgray: "rgb(129,129,129)",
       },
-      fontFamily:{
-        robotoExt:['Roboto', 'sans-serif']
+      fontFamily: {
+        robotoExt: ["Roboto", "sans-serif"],
       },
-      screens:{
-        yt2xs:'480px',
-        ytxs:'530px',
-        ytsm:'615px',
-        ytmd:'935px',
-        ytlg:'1150px',
-        ytxl:'1365px',
-        yt2xl:'1580px',
-        yt3xl:'1800px'
+      screens: {
+        yt2xs: "480px",
+        ytxs: "530px",
+        ytsm: "615px",
+        ytmd: "935px",
+        ytlg: "1150px",
+        ytxl: "1365px",
+        yt2xl: "1580px",
+        yt3xl: "1800px",
       },
-      spacing:{
-        '13':'3.25rem'
-      }
-    }
-  }
-}
+      spacing: {
+        "13": "3.25rem",
+      },
+      width: {
+        inherit: "inherit",
+      },
+    },
+  },
+};
