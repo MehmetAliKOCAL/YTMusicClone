2f05e48c9169c545d815bb8c5bf1cd12f4883f69
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-05-31 21:05:32 +0300

    style improvements on top search result

--- pages/search/[query].vue
+++ pages/search/[query].vue
@@ -1,17 +1,116 @@
 <script setup>
 const query = useRoute().params.query;
-let searchResult = ref(await $fetch(`/api/search/${query}`));
+let searchResult = ref(await $fetch(`/api/search?query=${query}&limit=20`));
+let tabSelected = ref(0);
+
+const categories = [
+  {
+    name: "Featured playlists",
+    link: "/",
+  },
+  {
+    name: "Songs",
+    link: "/",
+  },
+  {
+    name: "Vides",
+    link: "/",
+  },
+  {
+    name: "Artists",
+    link: "/",
+  },
+  {
+    name: "Community playlists",
+    link: "/",
+  },
+  {
+    name: "Albums",
+    link: "/",
+  },
+];
+
+const tabs = [{ text: "YT MUSIC" }, { text: "LIBRARY" }];
 </script>
 <template>
-  <div>
-    <p class="text-white mb-10">search query: {{ query }}</p>
-    <div class="text-white" v-for="result in searchResult?.items">
-      <p>{{ result?.title }}</p>
-      <p>{{ result?.id }}</p>
-      <p>url: {{ result?.url }}</p>
-      <p>author: {{ result?.author?.name }}</p>
-      <img :src="result?.author?.bestAvatar?.url" alt="" />
-      <img :src="result?.bestThumbnail?.url" :alt="result?.title" />
+  <main class="mx-auto max-w-[850px]">
+    <div
+      class="flex text-white text-sm font-bold space-x-8 border-b border-white/10"
+    >
+      <button
+        v-for="tab in tabs"
+        :key="tab"
+        @click="tabSelected = tabs.indexOf(tab)"
+        class="pb-3.5 border-b-2 transition-colors duration-300"
+        :class="[
+          tabSelected === tabs.indexOf(tab)
+            ? 'text-white border-white'
+            : 'text-white/40 border-transparent',
+        ]"
+      >
+        {{ tab.text }}
+      </button>
     </div>
-  </div>
+    <div v-if="tabSelected === 0">
+      <Categories :categories="categories" class="my-1.5" />
+      <h1 class="text-white font-black text-2xl mb-4">Top result</h1>
+      <div
+        class="text-white"
+        v-for="result in searchResult?.items"
+        :key="result"
+      >
+        <div
+          v-if="result === searchResult?.items[0]"
+          class="overflow-hidden rounded-lg relative h-32"
+        >
+          <div
+            :style="'background-image:url(' + result?.bestThumbnail?.url + ');'"
+            class="bg-center bg-[length:200%] filter blur-3xl h-32 bg-red-900"
+          />
+          <div
+            class="z-10 top-0 left-5 h-full flex absolute justify-center items-center text-white space-x-5"
+          >
+            <img
+              :src="result?.author?.bestAvatar?.url"
+              :alt="result?.title"
+              width="90"
+              class="rounded-lg"
+            />
+            <div class="flex flex-col justify-center">
+              <p class="text-2xl font-bold truncate">{{ result?.title }}</p>
+              <div class="flex space-x-1 text-white/70">
+                <p class="truncate">
+                  {{ result?.type.at(0).toUpperCase() + result?.type.slice(1) }}
+                </p>
+                <p>•</p>
+                <p class="hover:underline cursor-pointer">
+                  {{ result?.author?.name }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p>url: {{ result?.url }}</p>
+
+        <div v-if="result !== searchResult?.items[0]">
+          <p>{{ result?.type }}</p>
+          <p>{{ result?.title }}</p>
+          <p>{{ result?.id }}</p>
+          <p>url: {{ result?.url }}</p>
+          <p>author: {{ result?.author?.name }}</p>
+          <img :src="result?.author?.bestAvatar?.url" alt="" />
+          <img :src="result?.bestThumbnail?.url" :alt="result?.title" />
+        </div>
+      </div>
+    </div>
+    <div
+      v-else
+      class="mt-4 flex flex-col justify-center items-center space-y-4"
+    >
+      <div class="w-24">
+        <IconsSearch />
+      </div>
+      <p class="text-white/50 text-xl">No results found</p>
+    </div>
+  </main>
 </template>
