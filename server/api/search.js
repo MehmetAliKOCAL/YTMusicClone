cdbfdb03373c8cd258ca315f4cc33879e5496522
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-05-31 21:05:05 +0300

    added a middleware for searching with ytsr

---
+++ server/api/search.js
@@ -0,0 +1,9 @@
+import ytsr from "ytsr";
+
+export default defineEventHandler(async (event) => {
+  const searchQuery = getQuery(event);
+
+  return await ytsr(decodeURIComponent(searchQuery.query), {
+    limit: searchQuery.limit,
+  });
+});
--- server/api/search/[query].js
+++
@@ -1,7 +0,0 @@
-import ytsr from "ytsr";
-
-export default defineEventHandler(async (event) => {
-  return await ytsr(decodeURIComponent(event.context.params.query), {
-    limit: 3,
-  });
-});
