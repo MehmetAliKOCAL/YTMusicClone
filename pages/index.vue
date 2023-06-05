78cb6105fbdcd656287d0b6b939c746f64b37671
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-06-05 23:34:46 +0300

    youtube video ids added to static songs

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <script setup>
 const carouselItems = [
   {
+    type: "video",
+    name: "АДЛИН - DEAD INSIDE (slowed + reverb)",
+    artists: [
+      {
+        name: "SADHOURS",
+        link: "/",
+      },
+    ],
+    image: "https://img.youtube.com/vi/LkwbEp9t8ts/maxresdefault.jpg",
+    link: "/",
+    id: "MonFx8pX6U4",
+    views: "10M",
+  },
+  {
     type: "music",
     name: "UNFAZED",
     artists: [
@@ -12,6 +26,7 @@ const carouselItems = [
     image:
       "https://lh3.googleusercontent.com/KKt33I_H9O5AnHs6i1Ey1qUcK9bzkEAx-6jap1OLkJ4dDpQvm-D9hnk_LtH-8FKwcYMVb0mplEtAz-Z2PQ=w544-h544-l90-rj",
     link: "/",
+    id: "8fDtmYirdyI",
     album: "Single",
   },
   {
@@ -26,6 +41,7 @@ const carouselItems = [
     image:
       "https://i.ytimg.com/vi/lIB9exiMDLk/sddefault.jpg?sqp=-oaymwEWCJADEOEBIAQqCghqEJQEGHgg6AJIWg&rs=AMzJL3k0b6iNayMQ9qDpqjCxsna997yOAg",
     link: "/",
+    id: "lIB9exiMDLk",
     views: "144K",
   },
   {
@@ -44,6 +60,7 @@ const carouselItems = [
     image:
       "https://lh3.googleusercontent.com/jTuV7IX6XuAmqxiSW-K0l2-06QhasGdVp3yV41EJzYNRV2OnMPrvS_dnT4SOQTYkAeZ1Mp1gSjjUskDq=w544-h544-l90-rj",
     link: "/",
+    id: "R19vOXHXTkc",
     album: "Single",
   },
   {
@@ -58,6 +75,7 @@ const carouselItems = [
     image:
       "https://lh3.googleusercontent.com/3ps81ZHCkeH5prAobz5gU1AAYoM6qE6g64CaF7ytcfaxbRS7d3_T7N_DkeJauhUpCNltBO-8LG1kvUCV=w544-h544-l90-rj",
     link: "/",
+    id: "Tj9RZ17FcQY",
     album: "Song",
     explicit: true,
   },
@@ -72,7 +90,8 @@ const carouselItems = [
     ],
     image:
       "https://lh3.googleusercontent.com/2Km9wdDhevIp1rK1dlhRFgSGmMH5T0FhgX4DLwtJ3mGucJlIINmCO8rQDWcA-shuMQiSHFVd6RzcTLs=w544-h544-l90-rj",
-    link: "https://music.youtube.com/watch?v=bQQ0cZk8dW8&list=RDAMVMTj9RZ17FcQY",
+    link: "https://music.youtube.com/watch?v=ZhOODDxkTP0&list=OLAK5uy_nqq0E0Jk0dx_JrmTXb_jsnN43UgNhwJhA",
+    id: "ZhOODDxkTP0",
     album: "Single",
   },
   {
@@ -87,6 +106,7 @@ const carouselItems = [
     image:
       "https://i.ytimg.com/vi/gailByDebJc/sddefault.jpg?sqp=-oaymwEWCJADEOEBIAQqCghqEJQEGHgg6AJIWg&rs=AMzJL3mrdjDWQz_E1wEw7-Dggm6J9LJ6lg",
     link: "/",
+    id: "gailByDebJc",
     views: "230K",
   },
   {
@@ -101,6 +121,7 @@ const carouselItems = [
     image:
       "https://lh3.googleusercontent.com/xX1ZX4MUV7Gl6p9HkoEFZQAsafwaJB1VwDorEjp106wl1LTAMoMYEIw-cm392nMMTHVLI8M9A0x1iWie=w544-h544-l90-rj",
     link: "/",
+    id: "WIb5udLAheY",
     album: "Single",
     explicit: true,
   },
@@ -116,6 +137,7 @@ const carouselItems = [
     image:
       "https://lh3.googleusercontent.com/QEbU6zBoCgjoNtfnHWp5rP3uJRAE_is9_aRpulOOhDgMbl3dw44pOI5XBa3BcbHlBKc3yBxkPXYn6M8=w226-h226-l90-rj",
     link: "/",
+    id: "0wMKZ9PUm60",
     album: "Song",
   },
   {
@@ -130,6 +152,7 @@ const carouselItems = [
     image:
       "https://lh3.googleusercontent.com/Upi0Im7Cd8KNSwkdFiZzRrbJZFVSoLL95Emf0b1SIXA5gJhYheh6xnkEFhBazREm6wN4vCd7UeXhpDue=w226-h226-l90-rj",
     link: "/",
+    id: "3Qnfao8lrZo",
     album: "Single",
     explicit: true,
   },
@@ -145,6 +168,7 @@ const carouselItems = [
     image:
       "https://i.ytimg.com/vi/sADmwWhU5ZM/sddefault.jpg?sqp=-oaymwEWCJADEOEBIAQqCghqEJQEGHgg6AJIWg&rs=AMzJL3nQY1TzzrAN080hAWj2_IWruZ29Ug",
     link: "/",
+    id: "sADmwWhU5ZM",
     views: "74M",
   },
   {
@@ -163,6 +187,7 @@ const carouselItems = [
     image:
       "https://lh3.googleusercontent.com/U1XH8mpQPHmt-M1wcWJUYfr5fCtRSMgyVb8D6OqxDSf28bjjgN9O8QYDkTO7_F3dy-VbWqDNKhJOiB3f=w544-h544-l90-rj",
     link: "/",
+    id: "LouQ9gj_Crc",
     album: "Single",
     explicit: true,
   },
@@ -178,6 +203,7 @@ const carouselItems = [
     image:
       "https://lh3.googleusercontent.com/QUFdNC0Ua3RvFSo_MvV7dxYY07C3spS8VFtbfTw4Wf-3pooy1GrEZ_DMlMk3niCkA4CrQJ0Vj5iC6f_7zg=w226-h226-l90-rj",
     link: "/",
+    id: "8Rr8NS2Bm4U",
     album: "Song",
     explicit: true,
   },
@@ -192,6 +218,7 @@ const carouselItems = [
     image:
       "https://lh3.googleusercontent.com/npACGZxDWr77-RKaHUF1zbYizwSK9cysQ-WDtiM3XeUcdcIrz7QkBQXS2V7xLOrcF8eX_W2riGTxhgJ-HQ=w226-h226-l90-rj",
     link: "/",
+    id: "8_SrViAxIL0",
     album: "Single",
     explicit: true,
   },
@@ -202,6 +229,7 @@ const carouselItems = [
     image:
       "https://i.ytimg.com/vi/TkWjvgzu6Os/sddefault.jpg?sqp=-oaymwEWCJADEOEBIAQqCghqEJQEGHgg6AJIWg&rs=AMzJL3nMPhA3lRQ0NqEshikdHgKzzx5kIg",
     link: "/",
+    id: "9tUg46n9KPs",
     views: "12K",
   },
 ];
@@ -214,7 +242,7 @@ const carouselItems = [
       :image="{
         path: '/images/pfp.jpg',
         redirect: '/',
-        alt: 'user profile picture',
+        alt: 'GwynDev Profile Picture',
         showImage: true,
         isRounded: true,
       }"
